21eaaaa001d3cfc980228362c5b6f3878e4ad5f8
entreri514 <148565529+entreri514@users.noreply.github.com>
2024-02-10 15:40:46 -0600

    Attempting to debug src problem on ProductItem component while trying to display multiple products.

--- ReactJWTFrontend_Starter-main/src/components/ProductItem/ProductItem.jsx
+++ ReactJWTFrontend_Starter-main/src/components/ProductItem/ProductItem.jsx
@@ -9,6 +9,7 @@ const ProductItem = () => {
   const [item, setItem] = useState([]);
   const [isAdd, setIsAdd] = useState(false);
   const [user, token] = useAuth();
+  let displayResults;
 
   useEffect(() => {
     getItem();
@@ -18,21 +19,14 @@ const ProductItem = () => {
       const response = await axios.get(
         `https://localhost:5001/api/products/${atomicNumber}`
       );
-      console.log("setItem=", response.data[0]);
+      console.log("setItem=", response);
       if (response.status === 200) {
-        setItem(response.data[0]);
+        setItem(response.data);
       }
     } catch (error) {
       console.warn("Error submitting form ", error);
     }
   };
-
-  let thumb = `/pics/${item.itemPic}.jpg`;
-  if (item.itemType === "Plasma") thumb = `/pics/${item.itemPic}.mp4`;
-  console.log(thumb);
-  if (!item) {
-    return null;
-  }
   const handleSubmit = async (e) => {
     if (!isAdd) {
       e.preventDefault();
@@ -58,43 +52,50 @@ const ProductItem = () => {
     }
     setIsAdd(true);
   };
+  displayResults = item.map((item, index) => {
+    return (
+      <div>
+        {item.itemType === "Plasma" ? (
+          <video
+            autoPlay
+            loop
+            src={item.itemPic}
+            alt="video"
+            width="400"
+            height="450"
+            controls
+          />
+        ) : (
+          <img src={item.itemPic} alt="image" width="350" height="450" />
+        )}
+        <div className="product-description">
+          <h5>Atomic number: {item.atomicNumber}</h5>
+          <h5>Symbol: {item.symbol}</h5>
+          <h5>Product: {item.productInfo}</h5>
+          <h5>Care: {item.productCare}</h5>
+          <h5>Density: {item.density}</h5>
+          <h5>Melting Point: {item.meltingPoint}</h5>
+          <h5>Boiling Point: {item.boilingPoint}</h5>
+          <div></div>
+          <h3>Price: ${item.price}.00</h3>
+        </div>
+        {isAdd ? (
+          <button className="btn-isadd" type="submit">
+            Add to Cart
+          </button>
+        ) : (
+          <button className="btn-add" type="submit" onClick={handleSubmit}>
+            Add to Cart
+          </button>
+        )}
+      </div>
+    );
+  });
 
   return (
     <div className="product-details">
       <h2 className="title-item">{item.name}</h2>
-      {item.itemType === "Plasma" ? (
-        <video
-          autoPlay
-          loop
-          src={thumb}
-          alt="video"
-          width="400"
-          height="450"
-          controls
-        />
-      ) : (
-        <img src={thumb} alt="image" width="350" height="450" />
-      )}
-      <div className="product-description">
-        <h5>Atomic number: {item.atomicNumber}</h5>
-        <h5>Symbol: {item.symbol}</h5>
-        <h5>Product: {item.productInfo}</h5>
-        <h5>Care: {item.productCare}</h5>
-        <h5>Density: {item.density}</h5>
-        <h5>Melting Point: {item.meltingPoint}</h5>
-        <h5>Boiling Point: {item.boilingPoint}</h5>
-        <div></div>
-        <h3>Price: ${item.price}.00</h3>
-      </div>
-      {isAdd ? (
-        <button className="btn-isadd" type="submit">
-          Add to Cart
-        </button>
-      ) : (
-        <button className="btn-add" type="submit" onClick={handleSubmit}>
-          Add to Cart
-        </button>
-      )}
+      {displayResults}
     </div>
   );
 };
